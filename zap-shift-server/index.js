ba9a4367c6aea622dd171fbd4e0bf46ac8087da7
Rhcoder79 <robiulhasan7972171@gmail.com>
2026-05-05 21:09:47 +0600

    module 63 7no video paymenet success page creteing

---
+++ zap-shift-server/index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const cors=require('cors');
+const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+const port =process.env.PORT || 3000
+//middleware
+app.use(express.json());
+app.use(cors());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sumux0b.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db=client.db('zap_shift_db');
+    const parcelsCollection=db.collection('parcels');
+    app.get('/parcels',async(req,res)=>{
+     const query={};
+     const {email}=req.query;
+
+     if(email){
+        query.SenderEmail=email;
+     }
+     const options={sort:{createdAt:-1}}
+     const cursor=parcelsCollection.find(query,options);
+     const result=await cursor.toArray();
+     res.send(result);
+
+    })
+    app.get('/parcels/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await parcelsCollection.findOne(query);
+      res.send(result);
+    })
+    app.post('/parcels',async(req,res)=>{
+        const parcel=req.body;
+        parcel.createdAt=new Date();
+        const result=await parcelsCollection.insertOne(parcel);
+        res.send(result);
+    })
+    app.delete('/parcels/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await parcelsCollection.deleteOne(query);
+      res.send(result);
+    })
+    //PAYMENT RELATED APIS
+    app.post('/create-checkout-session',async(req,res)=>{
+      const paymentInfo=req.body;
+      const amount=parseInt(paymentInfo.cost)*100;
+      const session=await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price_data:{
+   currency:'USD',
+   unit_amount:amount,
+   product_data:{
+    name:paymentInfo.parcelName
+   }
+        },
+        quantity: 1,
+      },
+    ],
+    customer_email:paymentInfo.SenderEmail,
+    mode: 'payment',
+    metadata:{
+      parcelId:paymentInfo.parcelId
+    },
+    success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+    cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+  })
+  console.log(session);
+  res.send({url:session.url})
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('ZAP IS SHIFTING SHIFTING')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
